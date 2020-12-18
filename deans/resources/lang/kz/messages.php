e2e90d5521ec6b57e7615565731a19eb3bc94519
Karim0 <karim20.ks@gmail.com>
2020-12-18 22:35:50 +0600

    1

---
+++ deans/resources/lang/kz/messages.php
@@ -0,0 +1,94 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'deans' => 'Деканат',
+    'home' => 'Басты бет',
+    'news' => 'Жаңалықтар',
+    'contact' => 'Байланыс',
+    'send' => 'Жіберу',
+    'load_doc' => 'Документті жүктеу',
+    'in_process' => 'Процессте',
+    'my_ref' => 'Менің анықтамаларым',
+    'admin_panel' => 'Администратор бөлімі',
+    'exit' => 'Шығу',
+    'my_group' => 'Менің топтарым',
+    'req_ref' => 'Сұраныстағы анықтамалар',
+
+    'login' => 'Логин',
+    'name' => 'Есім',
+    'lastname' => 'Фамилия',
+    'patronymic' => 'Әкесінің аты',
+    'gender' => 'Жынысы',
+    'phone' => 'Телефон',
+    'birthday' => 'Туған күні',
+    'address_reg' => 'Тіркелген мекен-жайы',
+    'address_res' => 'Тұрғылықты мекен-жайы',
+    'iin' => 'ЖСН',
+
+    'drop_pass' => 'Құпиясөзді түсіру',
+    'reset_pass' => 'Құпиясөзді өзгерту',
+    'authorization' => 'Кіру',
+    'registration' => 'Тіркелу',
+    'edit'=>'Түзету',
+    'change' =>'Өзгерту',
+    'load' => 'Жүктеу',
+    'choose_file' => 'Файлды таңдаңыз',
+    'choose_image' => 'Суретті таңдаңыз',
+    'repeat_pass' => 'Құпиясөзді қайталаңыз',
+    'new_pass' => 'Жаңа құпиясөз',
+    'old_pass' => 'Ескі құпиясөз',
+    'add' => 'Қосу',
+    'created' => 'Құрылды',
+    'students_amount' => 'Студенттер саны',
+    'department' => 'Департамент',
+    'department_type' => 'Департамент түрлері',
+    'departments' => 'Департаменттер',
+    'add_student' => 'Студентті қосу',
+    'group' => 'Группа',
+    'groups' => 'Группы',
+    'student_status' => 'Студенттің мәртебесі',
+    'study_form' => 'Оқу формасы',
+    'study_lang' => 'Оқу тілі',
+    'payment_form' => 'Төлем түрі',
+    'course' => 'Курс',
+    'student_info' => 'Студент туралы ақпарат',
+    'yes' => 'Ия',
+    'no' => 'Жоқ',
+    'foreign' => 'Шет елдің адамы',
+    'academic_rank' => 'Ғылыми дәреже',
+    'academic_degree' => 'Ғылыми деңгей',
+    'english_level' => 'Ағылшын тілінің деңгейі',
+    'staff_info' => 'Жұмысшы туралы ақпарат',
+    'references' => 'Анықтамалар',
+    'degree_type' => 'Деңгей',
+    'student_login' => 'Студенттің логині',
+
+    'description_en' => 'Ағылшын тілінде сипаттама',
+    'description_ru' => 'Орыс тілінде сипаттама',
+    'description_kz' => 'Қазақ тілінде сипаттама',
+    'title_en' => 'Ағылшын тілінде атауы',
+    'title_ru' => 'Орыс тілінде атауы',
+    'title_kz' => 'Қазақ тілінде атауы',
+    'abbreviation_en' => 'Ағылшын тілінде қысқарма',
+    'abbreviation_ru' => 'Орыс тілінде қысқарма',
+    'abbreviation_kz' => 'Қазақ тілінде қысқарма',
+    'receipt_date' => 'Оқуға түскен күні',
+    'staff_amount' => 'Жұмысшылар саны',
+    'type' => 'Түрі',
+    'refers_to' => 'Қатысты',
+    'users'=>'Қолданушылар',
+    'role'=>'Рөл',
+    'description'=>'Сипаттама'
+];
